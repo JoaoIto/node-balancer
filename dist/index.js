bcbb52c42604d7cfe67028d4a6712c83356f0678
JoaoIto <joaovictorpfr@gmail.com>
2026-02-13 11:54:01 -0300

    chore: stop tracking dist folder

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConnectionManager = void 0;
-var connectionManager_1 = require("./config/connectionManager");
-Object.defineProperty(exports, "ConnectionManager", { enumerable: true, get: function () { return connectionManager_1.ConnectionManager; } });
--- dist/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const router = (0, express_1.Router)();
-const controller = new user_controller_1.UserController();
-router.post('/', controller.create);
-router.get('/', controller.list);
-exports.default = router;
